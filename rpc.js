15b057bd803945dae8970144408211113bd4f771
Thund33r <121821296+thund33r-new@users.noreply.github.com>
2023-01-03 01:59:22 +0300

    Update rpc.js

--- rpc.js
+++ rpc.js
@@ -1,17 +1,16 @@
 const { Client, SpotifyRPC, CustomStatus, RichPresence } = require('discord.js-selfbot-v13');
-const keep_alive = require('./keep_alive.js');
 const client = new Client({ checkUpdate: false });
 
-client.login(токен)
+client.login('токен')
 client.on('ready', () => {const r = new RichPresence(client)
     .setAssetsLargeImage("Ссылка на картинку") // Картинка
-    .setAssetsLargeText('') // Текст при наводке
+    .setAssetsLargeText('') // Текст при наводке на картинку
     .setType("STREAMING")
     .setURL("ссылка стрима")  
-    .setApplicationId('айди приложения') // Можете даже не заполнять
+    .setApplicationId('айди приложения')
     .addButton("название кнопки", "ссылка") // Кнопка
     .addButton("название кнопки2", "ссылка2") // Кнопка 2
-    .setName("TWITCH") // Строка где "Стримит на <name>"
+    .setName("TWITCH") // Строка где "Стримит на 
     .setDetails('') // Верхний текст.
 
 client.user.setPresence({ activities: [r.toJSON()] })})
